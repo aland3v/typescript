608233411c4fe4f5ec041637fecabc5b14fea0ca
Alan dev <aland3v@gmail.com>
2020-12-26 11:54:05 -0400

    Void, Never and Any

---
+++ src/type-any.ts
@@ -0,0 +1,20 @@
+// Tipo explicito
+let idUser: any;
+idUser = 1;
+idUser = '1';
+console.log('idUser:', idUser);
+
+// Tipo inferido
+let otherId;
+otherId = 1;
+otherId = '1';
+otherId = true;
+console.log('otherId:',otherId);
+
+
+let surprise: any = 'hello typescript';
+// podria haber error en tiempo de ejecucion
+// si encaso el valor de surprise fuera otro
+// distintio al string
+const res = surprise.substring(6)
+console.log(res);
---
+++ src/type-void.ts
@@ -0,0 +1,51 @@
+// Void
+
+// Tipo explicito
+function showInfo(user: any): any{
+    console.log('User info',user.id, user.username, user.firstName);
+}
+
+showInfo({id: 1, username: 'quispe', firstName: 'alan'});
+
+// Tipo inferido
+function showFormattedInfo(user: any){
+    console.log('User Info',`
+        id: ${user.id}
+        username: ${user.username}
+        firstName: ${user.firstName}
+    `);
+}
+
+showFormattedInfo({id: 1, username: 'quispe', firstName: 'alan'});
+
+// tipo void, como tipo de dato en variable
+// null y undefined son especie de subtipos para
+// el tipo void
+// antes debemos dar "strict": false, en el tsconfig
+let unusable: void;
+unusable = null;
+unusable = undefined;
+
+// Tipo: Never
+function handleError(code: number, message: string): never{
+    // Process your code here
+    // Generate a message
+    throw new Error(`${message}. Code: ${code}`);
+    // Solo lanza un error
+}
+
+try {
+    handleError(404, 'Not Found');
+} catch (error) {
+    console.log('gestionando error')
+}
+
+function sumNumbers(limit: number): never{
+    let sum = 0;
+    while(true) {
+        sum++
+    }
+    // nunca retornaremos la suma acumulada
+}
+
+sumNumbers(10);
