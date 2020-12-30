8e0b995b8ef37e1e75c8532be20b83071b82078a
Alan dev <aland3v@gmail.com>
2020-12-30 12:52:02 -0400

    Herencia entre clases

---
+++ dist/clases-herencia.js
@@ -0,0 +1,67 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var PhotoOrientation;
+(function (PhotoOrientation) {
+    PhotoOrientation[PhotoOrientation["Landscape"] = 0] = "Landscape";
+    PhotoOrientation[PhotoOrientation["Portrait"] = 1] = "Portrait";
+    PhotoOrientation[PhotoOrientation["Square"] = 2] = "Square";
+    PhotoOrientation[PhotoOrientation["Panorama"] = 3] = "Panorama";
+})(PhotoOrientation || (PhotoOrientation = {}));
+// Superclase
+class Item {
+    constructor(id, title) {
+        this._id = id;
+        this._title = title;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(id) {
+        this._id = id;
+    }
+    get title() {
+        return this._title;
+    }
+    set title(title) {
+        this._title = title;
+    }
+}
+// get y set
+class Picture extends Item {
+    constructor(id, title, orientation) {
+        super(id, title);
+        this._orientation = orientation;
+    }
+    get orientation() {
+        return this._orientation;
+    }
+    set orientation(value) {
+        this._orientation = value;
+    }
+    // Comportamiento
+    toString() {
+        return `[id: ${this.id},
+                 title: ${this.title},
+                 orientation: ${this.orientation}]`;
+    }
+}
+class Album extends Item {
+    constructor(id, title) {
+        super(id, title);
+        this.pictures = [];
+    }
+    addPicture(picture) {
+        this.pictures.push(picture);
+    }
+}
+// Instanciación
+const album = new Album(1, 'Personal Pic');
+const picture = new Picture(1, 'Platzi session', PhotoOrientation.Square);
+album.addPicture(picture);
+console.log('album', album);
+// Accediendo a los miembros publicos
+console.log('picture.id', picture.id); // internamente get id()
+picture.id = 100; // internamente, set id(100)
+picture.title = 'Another title';
+album.title = 'Personal Activities';
+console.log('album', album);
---
+++ src/clases-herencia.ts
@@ -0,0 +1,91 @@
+export {};
+
+enum PhotoOrientation {
+    Landscape,
+    Portrait,
+    Square,
+    Panorama
+}
+
+// Superclase
+class Item {
+    protected _id: number;
+    protected _title: string;
+
+    constructor(id: number, title: string) {
+        this._id = id;
+        this._title = title;
+    }
+
+    get id() {// metodo y atributo no deben tener igual identificador
+        return this._id;
+    }
+
+    set id(id: number) {
+        this._id = id;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    set title(title: string) {
+        this._title = title;
+    }
+
+}
+
+// get y set
+class Picture extends Item{
+    // Propiedades
+    private _orientation: PhotoOrientation;
+
+    public constructor(id: number,
+        title: string,
+        orientation: PhotoOrientation) {
+            super(id, title);
+            this._orientation = orientation;
+    }
+
+    public get orientation(): PhotoOrientation {
+        return this._orientation;
+    }
+
+    public set orientation(value: PhotoOrientation) {
+        this._orientation = value;
+    }
+
+    // Comportamiento
+    public toString(){
+        return `[id: ${this.id},
+                 title: ${this.title},
+                 orientation: ${this.orientation}]`
+    }
+}
+
+class Album extends Item {
+    private pictures: Picture[];
+
+    public constructor(id:number,
+        title: string) {
+            super(id, title);
+            this.pictures = [];
+    }
+
+    public addPicture(picture: Picture) {
+        this.pictures.push(picture);
+    }
+}
+
+// Instanciación
+const album: Album = new Album(1, 'Personal Pic');
+const picture: Picture = new Picture(1, 'Platzi session', PhotoOrientation.Square);
+album.addPicture(picture);
+console.log('album', album);
+
+// Accediendo a los miembros publicos
+console.log('picture.id', picture.id); // internamente get id()
+picture.id = 100; // internamente, set id(100)
+picture.title = 'Another title';
+album.title = 'Personal Activities';
+console.log('album', album);
